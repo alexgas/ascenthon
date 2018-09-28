41ea1d274d1810c1365d0e6bb0f28990cae09163
Alex <alexlpdj@gmail.com>
2018-09-29 01:51:20 +0200

    image to base 64

--- js/reconocimiento.js
+++ js/reconocimiento.js
@@ -1,45 +1,117 @@
 $( function (){
 
-        let video = document.getElementById('videoInput');
-        let streaming = false;
-        let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-        let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
-        let cap = new cv.VideoCapture(video);
+    let streaming = false;
+    let video = document.getElementById('videoInput');
+    let utils = new Utils('errorMessage'); //use utils class
+    let classifier = new cv.CascadeClassifier();
+    let faceCascadeFile = 'haarcascade_frontalface_default.xml';
+    let contador = 0;
 
-        const FPS = 30;
-        function processVideo() {
+    utils.createFileFromUrl(faceCascadeFile, faceCascadeFile, () => {
+        classifier.load(faceCascadeFile); // in the callback, load the cascade from file
+    });
 
-            console.log('Streaming: ' + streaming);
+
+
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(function(stream) {
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(function(err) {
+            console.log("An error occured! " + err);
+        });
+
+        let faceImages = [];
+
+
+    function processVideo() {
+
+
+
+            let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+            let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+            let cap = new cv.VideoCapture(video);
+            let gray = new cv.Mat();
+            let faces = new cv.RectVector();
+
+            const FPS = 30;
 
             try {
                 if (!streaming) {
                     // clean and stop.
-                    console.log('clean');
                     src.delete();
                     dst.delete();
+                    gray.delete();
+                    faces.delete();
+                    faceImages = [];
                     return;
-                }
-                let begin = Date.now();
-                // start processing.
-                cap.read(src);
-                console.log('read');
+                } else {
+                    contador++;
+                    let begin = Date.now();
+                    // start processing.
+                    cap.read(src);
+                    src.copyTo(dst);
+                    cv.cvtColor(dst, gray, cv.COLOR_RGBA2GRAY, 0);
+                    // detect faces.
+                    // classifier.detectMultiScale(gray, faces, 1.1, 3, 0);
+                    // draw faces.
+                    /*
+                    for (let i = 0; i < faces.size(); ++i) {
+                        let face = faces.get(i);
 
-                cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-                cv.imshow('canvasOutput', dst);
-                // schedule the next one.
-                let delay = 1000/FPS - (Date.now() - begin);
-                setTimeout(processVideo, delay);
+                        let point1 = new cv.Point(face.x, face.y);
+                        let point2 = new cv.Point(face.x + face.width, face.y + face.height);
+                        //cv.rectangle(dst, point1, point2, [255, 0, 0, 255]);
+
+                        let imagen = document.getElementById('canvasOutput');
+                        let base64 = imagen.toDataURL();
+
+                        faceImages.push(base64);
+
+                        /*
+                        var image = new Image();
+                        image.src = base64;
+                        var canvas = document.createElement("canvas");
+                        var context = canvas.getContext('2d');
+                       // set canvas dimensions
+                        canvas.width = face.width;
+                        canvas.height = face.height;
+
+                        image.onload = function() {
+                            context.drawImage(image, face.x, face.y, face.width, face.height, 0, 0, face.width, face.height);
+                        };
+
+
+                    }*/
+
+                    cv.imshow('canvasOutput', dst);
+
+                    let imagen = document.getElementById('canvasOutput');
+                    let base64 = imagen.toDataURL();
+
+                    faceImages.push(base64);
+
+                    // schedule the next one.
+                    let delay = 1000 / FPS - (Date.now() - begin);
+                    setTimeout(processVideo, delay);
+                }
             } catch (err) {
                 console.dir('Error: ' + err);
             }
-        };
 
-// schedule the first one.
-        setTimeout(processVideo, 0);
+    };
 
-    $('#toggleCapture').on('click', () => {
-       streaming = !streaming;
+    // schedule the first one.
+    //setTimeout(processVideo, 0);
+
+
+    $('#button-hide-door').on('click', () => {
+        streaming = !streaming;
+        console.log('imagenes: ' + JSON.stringify(faceImages));
+
         processVideo();
+
     });
 
 });
