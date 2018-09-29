16f780a3046991becc0bb7585db9902c8d1a65ca
Alex <alexlpdj@gmail.com>
2018-09-29 13:22:08 +0200

    fix canvas output

--- js/reconocimiento.js
+++ js/reconocimiento.js
@@ -27,8 +27,6 @@ $( function (){
 
     function processVideo() {
 
-
-
             let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
             let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
             let cap = new cv.VideoCapture(video);
@@ -50,48 +48,31 @@ $( function (){
                 } else {
                     contador++;
                     let begin = Date.now();
-                    // start processing.
+
                     cap.read(src);
                     src.copyTo(dst);
                     cv.cvtColor(dst, gray, cv.COLOR_RGBA2GRAY, 0);
-                    // detect faces.
-                    // classifier.detectMultiScale(gray, faces, 1.1, 3, 0);
-                    // draw faces.
-                    /*
-                    for (let i = 0; i < faces.size(); ++i) {
-                        let face = faces.get(i);
 
-                        let point1 = new cv.Point(face.x, face.y);
-                        let point2 = new cv.Point(face.x + face.width, face.y + face.height);
-                        //cv.rectangle(dst, point1, point2, [255, 0, 0, 255]);
+                    cv.imshow('canvasOutputNoFaces', dst);
 
-                        let imagen = document.getElementById('canvasOutput');
-                        let base64 = imagen.toDataURL();
-
-                        faceImages.push(base64);
-
-                        /*
-                        var image = new Image();
-                        image.src = base64;
-                        var canvas = document.createElement("canvas");
-                        var context = canvas.getContext('2d');
-                       // set canvas dimensions
-                        canvas.width = face.width;
-                        canvas.height = face.height;
-
-                        image.onload = function() {
-                            context.drawImage(image, face.x, face.y, face.width, face.height, 0, 0, face.width, face.height);
-                        };
-
-
-                    }*/
-
-                    //cv.imshow('canvasOutput', dst);
-
-                    let imagen = document.getElementById('canvasOutput');
+                    let imagen = document.getElementById('canvasOutputNoFaces');
                     let base64 = imagen.toDataURL();
 
                     faceImages.push(base64);
+
+                    // detectar caras.
+                    classifier.detectMultiScale(gray, faces, 1.1, 3, 0);
+
+
+
+                    // dibujar rectangulos en caras.
+                    for (let i = 0; i < faces.size(); ++i) {
+                        let face = faces.get(i);
+                        let point1 = new cv.Point(face.x, face.y);
+                        let point2 = new cv.Point(face.x + face.width, face.y + face.height);
+                        cv.rectangle(dst, point1, point2, [255, 0, 0, 255]);
+                    }
+                    cv.imshow('canvasOutput', dst);
 
                     // schedule the next one.
                     //let delay = 1000 / FPS - (Date.now() - begin);
@@ -106,8 +87,9 @@ $( function (){
 
     $('#button-hide-door').on('click', () => {
         streaming = !streaming;
-        processVideo();
         console.log(faceImages.length);
+        processVideo();
+
     });
 
 
